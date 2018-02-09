c38d3212ccc8e3b285cf6bfa7f90e83bf9943fd5
USER <USER@COM141-144>
2018-02-09 11:09:09 +0900

    Minusclass수정

--- Calc/src/MinusClass.java
+++ Calc/src/MinusClass.java
@@ -4,6 +4,6 @@ public class MinusClass implements Hap {
 	System.out.println("빼기 연산");
 	int num1 = nc.getNum1();
 	 int num2 = nc.getNum2();
-	 System.out.println(num1+"-"+num2+"="+(num1-num2));
+	 System.out.println(num1+"-"+num2+"="+(num1-num2)+"ㅋㅋㅋㅋ");
  }
 }
